8e9cb131de5ab26b00c1365ce190e9b69b0b545e
kernelidze <kernelidze@gmail.com>
2023-06-24 22:02:34 +0300

    Делу время

--- js/functions.js
+++ js/functions.js
@@ -12,3 +12,22 @@ isPalindrome('топот топот');
 const extractNumber = (string) => parseInt(String(string.replace(/\D/g, '')), 10);
 
 extractNumber('er 234');
+
+const isMeetingWorktime = (startWork, endWork, startMeeting, durationMeeting) => {
+  const startMeetingInMinutes = Number(startMeeting.split(':')[0]) * 60 + Number(startMeeting.split(':')[1]);
+  const startWorktimeInMinutes = Number(startWork.split(':')[0]) * 60 + Number(startWork.split(':')[1]);
+  const endWorktimeInMinutes = Number(endWork.split(':')[0]) * 60 + Number(endWork.split(':')[1]);
+  const worktimeTotal = startWorktimeInMinutes + endWorktimeInMinutes;
+
+  if (worktimeTotal < durationMeeting || startWorktimeInMinutes > startMeetingInMinutes) {
+    return false;
+  }
+
+  return endWorktimeInMinutes - startMeetingInMinutes >= durationMeeting;
+};
+
+isMeetingWorktime('08:00', '17:30', '14:00', 90); // true
+isMeetingWorktime('8:0', '10:0', '8:0', 120); // true
+isMeetingWorktime('08:00', '14:30', '14:00', 90); // false
+isMeetingWorktime('14:00', '17:30', '08:0', 90); // false
+isMeetingWorktime('8:00', '17:30', '08:00', 900); // false
